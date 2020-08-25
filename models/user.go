0e87bfc606ae281153646b7f89e7af92cd8aa092
realChainLife <realchainlife@protonmail.com>
2020-08-25 20:29:03 +0300

    add user models profile

---
+++ models/user.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username      string       `gorm:"unique;not null"`
+	WalletAddress string       `gorm:"unique;not null"`
+	Courses       []Course      `gorm:"foreignkey:ProtocolID"`
+	LearningPath  []LearningPath `gorm:"many2many:users_learningpath"`
+	DisplayName   string
+}
+
+type UserRepo interface {
+	Create(u *User) error
+	Update(u *User) error
+	UpdateLearningPath(u *User) error
+	GetByID(id uint) (*User, error)
+	GetByUsername(username string) (*User, error)
+	GetByWalletAddr(addr string) (*User, error)
+}
