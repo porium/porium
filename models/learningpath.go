0dc62753cc6ab9ebecc77007036f9526e9c1f78b
realChainLife <realchainlife@protonmail.com>
2020-08-29 18:06:01 +0300

    add learningpath model

--- models/learningpath.go
+++ models/learningpath.go
@@ -2,13 +2,12 @@ package models
 
 type Path struct {
 	Name   string  `gorm:"primary_key;auto_increment:false"`
-	Courses []Course `gorm:"many2many:tracks_instruments"`
-	Paths []Path
-	Users  []User  `gorm:"many2many:users_instruments"`
+	Courses []Course `gorm:"many2many:courses_path"`
+	Users  []User  `gorm:"many2many:users_path"`
 }
 
 type PathRepo interface {
-	GetByName(name string) (*Instrument, error)
-	Create(i *Instrument) error
-	Update(i *Instrument) error
+	GetByName(name string) (*Path, error)
+	Create(i *Path) error
+	Update(i *Path) error
 }
